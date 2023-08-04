0cef23ed8ec214bc0c20b1a8eb30f6bd1edcc181
azrielmt <azrielmtamayo@gmail.com>
2023-08-04 00:08:41 -0400

    Updated sql file

---
+++ Resources/newsql.sql
@@ -0,0 +1,37 @@
+drop table country_data
+
+CREATE TABLE country_data (
+    Country TEXT,
+    "Density (P/Km2)" TEXT,
+    "Agricultural Land (%)" TEXT,
+    "Land Area(Km2)" TEXT,
+    "Armed Forces size" TEXT,
+    "Birth Rate" TEXT,
+    "Co2-Emissions" TEXT,
+    CPI TEXT,
+    "CPI Change (%)" TEXT,
+    "Fertility Rate" FLOAT,
+    "Forested Area (%)" TEXT,
+    "Gasoline Price" TEXT,
+    GDP TEXT,
+    "Gross primary education enrollment (%)" TEXT,
+    "Gross tertiary education enrollment (%)" TEXT,
+    "Infant mortality" FLOAT,
+    "Life expectancy" FLOAT,
+    "Maternal mortality ratio" FLOAT,
+    "Minimum wage" TEXT,
+    "Out of pocket health expenditure" TEXT,
+    "Physicians per thousand" FLOAT,
+    "Population" TEXT,
+    "Population: Labor force participation (%)" TEXT,
+    "Tax revenue (%)" TEXT,
+    "Total tax rate" TEXT,
+    "Unemployment rate" TEXT,
+    "Urban_population" TEXT,
+    "Latitude" FLOAT,
+    "Longitude" FLOAT,
+    "Urban Percentage" TEXT,
+    "Life expectancy group" int
+);
+
+select * from country_data
